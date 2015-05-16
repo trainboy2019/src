92fd66e1ebd62cff805c91295a404a7b34fc2c0b
trainboy2019 <trainboy2019@users.noreply.github.com>
2015-05-16 17:25:03 -0400

    Update AmethystBlock.java

--- main/java/com/camp/block/AmethystBlock.java
+++ main/java/com/camp/block/AmethystBlock.java
@@ -2,8 +2,13 @@ package com.camp.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import com.camp.coloredlightscore.src.api.CLApi;
 
 import com.camp.lib.StringLibrary;
+
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class AmethystBlock extends Block{
 
@@ -11,6 +16,19 @@ public class AmethystBlock extends Block{
 		super(p_i45394_1_);
 		this.setBlockTextureName(StringLibrary.MODID + ":" + "amethyst" + "Block");
 		// TODO Auto-generated constructor stub
+		if (Loader.isModLoaded("coloredlightscore")) {
+            CLApi.setBlockColorRGB(this, 119, 0, 184);
+        } else {
+            setLightLevel(6F/15F);
+        }
 	}
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
+	public boolean isOpaqueCube() {
+		return false;
+		}
 
 }
