24db81688a9fa0059e574adb0f197a8a643893a9
trainboy2019 <trainboy2019@users.noreply.github.com>
2015-05-16 17:28:16 -0400

    Update PeridonBlock.java

--- main/java/com/camp/block/PeridonBlock.java
+++ main/java/com/camp/block/PeridonBlock.java
@@ -2,15 +2,32 @@ package com.camp.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import com.camp.coloredlightscore.src.api.CLApi;
 
 import com.camp.lib.StringLibrary;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class PeridonBlock extends Block{
 
-	protected PeridonBlock(Material p_i45394_1_) {
-		super(p_i45394_1_);
+	protected PeridonBlock(Material p_i45394_7_) {
+		super(p_i45394_7_);
 		this.setBlockTextureName(StringLibrary.MODID + ":" + "peridon" + "Block");
 		// TODO Auto-generated constructor stub
+		if (Loader.isModLoaded("coloredlightscore")) {
+            CLApi.setBlockColorRGB(this, 0, 255, 12);
+        } else {
+            setLightLevel(6F/15F);
+        }
 	}
-
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
+	public boolean isOpaqueCube() {
+		return false;
+		}
 }
