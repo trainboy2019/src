c32daa8daf58728616a0a448d845f4ea9e4ee82c
trainboy2019 <trainboy2019@users.noreply.github.com>
2015-05-16 17:25:30 -0400

    Update AquamarineBlock.java

--- main/java/com/camp/block/AquamarineBlock.java
+++ main/java/com/camp/block/AquamarineBlock.java
@@ -2,15 +2,32 @@ package com.camp.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import com.camp.coloredlightscore.src.api.CLApi;
 
 import com.camp.lib.StringLibrary;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class AquamarineBlock extends Block{
 
-	protected AquamarineBlock(Material p_i45394_1_) {
-		super(p_i45394_1_);
+	protected AquamarineBlock(Material p_i45394_2_) {
+		super(p_i45394_2_);
 		this.setBlockTextureName(StringLibrary.MODID + ":" + "aquamarine" + "Block");
 		// TODO Auto-generated constructor stub
+		if (Loader.isModLoaded("coloredlightscore")) {
+            CLApi.setBlockColorRGB(this, 0, 255, 204);
+        } else {
+            setLightLevel(6F/15F);
+        }
 	}
-
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
+	public boolean isOpaqueCube() {
+		return false;
+		}
 }
