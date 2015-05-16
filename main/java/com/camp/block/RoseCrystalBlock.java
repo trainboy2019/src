a7b448fd6fd6f1d8510c2264ac8462b3289c6797
trainboy2019 <trainboy2019@users.noreply.github.com>
2015-05-16 17:28:37 -0400

    Update RoseCrystalBlock.java

--- main/java/com/camp/block/RoseCrystalBlock.java
+++ main/java/com/camp/block/RoseCrystalBlock.java
@@ -2,15 +2,35 @@ package com.camp.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import com.camp.coloredlightscore.src.api.CLApi;
 
 import com.camp.lib.StringLibrary;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class RoseCrystalBlock extends Block{
 
-	protected RoseCrystalBlock(Material p_i45394_1_) {
-		super(p_i45394_1_);
+	protected RoseCrystalBlock(Material p_i45394_8_) {
+		super(p_i45394_8_);
 		this.setBlockTextureName(StringLibrary.MODID + ":" + "roseCrystal" + "Block");
 		// TODO Auto-generated constructor stub
+		if (Loader.isModLoaded("coloredlightscore")) {
+            CLApi.setBlockColorRGB(this, 252, 144, 244);
+            //CLApi.setBlockColorRGB(this, 26, 255, 1);
+            //System.out.println("\n\nTHIS WAS SET RIGHT!!!!!!!!!!!!!!!!!!\n\n!!!!!!");
+        } else {
+            setLightLevel(0F/15F);
+            //System.out.println("\n\nTHIS WAS SET WRONG!!!!!!!!!!!!!!!!!!\n\n!!!!!!");
+        }
 	}
-
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
+	public boolean isOpaqueCube() {
+		return false;
+		}
 }
