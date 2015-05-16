c65207d389bc64d8a682a6271d073d5079c48a70
trainboy2019 <trainboy2019@users.noreply.github.com>
2015-05-16 17:26:16 -0400

    Update CitrineBlock.java

--- main/java/com/camp/block/CitrineBlock.java
+++ main/java/com/camp/block/CitrineBlock.java
@@ -2,15 +2,33 @@ package com.camp.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import com.camp.coloredlightscore.src.api.CLApi;
 
 import com.camp.lib.StringLibrary;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class CitrineBlock extends Block{
 
-	protected CitrineBlock(Material p_i45394_1_) {
-		super(p_i45394_1_);
+	protected CitrineBlock(Material p_i45394_3_) {
+		super(p_i45394_3_);
 		this.setBlockTextureName(StringLibrary.MODID + ":" + "citrine" + "Block");
 		// TODO Auto-generated constructor stub
+		if (Loader.isModLoaded("coloredlightscore")) {
+            CLApi.setBlockColorRGB(this, 255, 186, 0);
+        } else {
+            setLightLevel(6F/15F);
+        }
 	}
-
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
+	public boolean isOpaqueCube() {
+		return false;
+		}
 }
+s
